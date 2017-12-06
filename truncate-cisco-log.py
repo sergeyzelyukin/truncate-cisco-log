98cb5b5b38c4863a7071b06f57e13f32074188f6
Sergey Zelyukin <neo@azdata.net>
2017-12-06 20:02:56 +0400

    recur change

--- truncate-cisco-log.py
+++ truncate-cisco-log.py
@@ -3,57 +3,47 @@
 import sys,os,datetime
 from LogLine import LogLine
 
-def recur():
-
-
-def main():
-  if not len(sys.argv)==4:
-    print "usage: {0} <LOG FILENAME> <MONTH> <DAY>".format(sys.argv[0])
-    sys.exit(1)
-  else:
-    log_filename = sys.argv.pop(1)
-
-  if not os.path.exists(log_filename):
-    print "there is no such file"
-    sys.exit(1)
-    
-  try:
-    target_date = datetime.datetime.strptime('{0} {1} 2017'.format(sys.argv.pop(1), sys.argv.pop(1)), "%m %d %Y").date()
-  except:
-    print "wrong date"
-    sys.exit(1)
-
-  source_filesize = os.path.getsize(log_filename)
-  
-  with open(log_filename, "rb") as source:
-    pos = source_filesize/2
-    source.seek(pos, 1)
+def recur(pos):
+  line = source.readline().strip()
+  logline = LogLine(line)
+  while not logline.ok:
     line = source.readline().strip()
     logline = LogLine(line)
-    while not logline.ok:
-      line = source.readline().strip()
-      logline = LogLine(line)
-    if logline.datetime.date() == target_date:
-      print line
-    elif logline.datetime.date() > target_date:
-      print line
-      source.seek(-pos/2, 1)
-      line = source.readline().strip()
-      logline = LogLine(line)
-      while not logline.ok:
-        line = source.readline().strip()
-        logline = LogLine(line)
-      print line
-    else:
-      print line
-      source.seek(pos/2, 1)
-      line = source.readline().strip()
-      logline = LogLine(line)
-      while not logline.ok:
-        line = source.readline().strip()
-        logline = LogLine(line)
-      print line
+  if logline.datetime.date() > target_date:
+    print line
+    pos=pos/2
+    source.seek(-pos, 1)
+    recur(pos)
+  elif logline.datetime.date() < target_date:
+    print line
+    pos=pos/2
+    source.seek(pos, 1)
+    recur(pos)
+  else:
+    print line
+    print "Search completed"
 
-if __name__ == "__main__":
-  main()
+
+if not len(sys.argv)==4:
+  print "usage: {0} <LOG FILENAME> <MONTH> <DAY>".format(sys.argv[0])
+  sys.exit(1)
+else:
+  log_filename = sys.argv.pop(1)
+
+if not os.path.exists(log_filename):
+  print "there is no such file"
+  sys.exit(1)
   
+try:
+  target_date = datetime.datetime.strptime('{0} {1} 2017'.format(sys.argv.pop(1), sys.argv.pop(1)), "%m %d %Y").date()
+except:
+  print "wrong date"
+  sys.exit(1)
+
+source_filesize = os.path.getsize(log_filename)
+
+with open(log_filename, "rb") as source:
+  pos = source_filesize/2
+  source.seek(pos, 1)
+  recur(pos)
+

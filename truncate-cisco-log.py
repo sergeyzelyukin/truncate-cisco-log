68c4d38fdac83b1169eab21e1628313c5f9149e0
Sergey Zelyukin <neo@azdata.net>
2017-12-07 23:06:29 +0400

    classes

--- truncate-cisco-log.py
+++ truncate-cisco-log.py
@@ -3,37 +3,123 @@
 import sys,os,datetime
 from LogLine import LogLine
 
-def read_until_logline(fh):
-  while True:
-    line_pos = fh.tell()
-    line = fh.readline()
-    if not line:
-      # EOF
-      return (-1, Null)
-    logline = LogLine(line)
-    if logline.ok:
-      return (line_pos, logline)
+class LogFile(file):
+  INITIAL_OFFSET = 4096
+  MAX_FAST_GROW = 512000
+  INCREMENTAL_GROW = 256000
+  
+  def __init__(self, _filename):
+    self._filename = _filename
+    self._date = None
+    self._logline = None
+    self._line_pos = None
+    super(LogFile,self).__init__(_filename, "rb")
+    self._filesize = os.path.getsize(_filename)
 
-
-def find_date(fh, offset):
-  line_pos, logline = read_until_logline(fh)
-  if line_pos<0:
-    # No logline found
-    return line_pos
+  def fast_rewind(self):
+    if not self._date:
+      raise Exception("no date given") 
     
-  if logline.datetime.date() == target_date:
-    # Search completed
-    return line_pos
-  else:
-    new_offset = offset/2
-    if new_offset == 0:
-      # not found
-      return -1
-    new_offset = -new_offset if logline.datetime.date() > target_date else new_offset
-    fh.seek(new_offset, 1)
-    return find_date(fh, abs(new_offset))
+    if not self._filesize > 0:
+      return False 
+
+    # Start from the middle
+    start_pos = self._filesize/2
+    self.seek(start_pos, 0)    
+    return self.recursive_jumps_to_date(start_pos)
+
+  def read_until_logline(self):
+    while True:
+      line_pos = self.tell()
+      line = self.readline()
+      if not line:
+        # EOF
+        return False
+      logline = LogLine(line)
+      if logline.ok:
+        # line is ok, save it
+        self._line_pos = line_pos
+        self._logline = logline
+        return True
+
+  def recursive_jumps_to_date(self, offset):
+    if not self.read_until_logline():
+      # No logline found
+      return False
+    
+    if self._logline.datetime.date() == self._date:
+      # Search completed
+      return True
+    else:
+      new_offset = offset/2
+      if new_offset == 0:
+        # no place no jump
+        return False
+
+      new_offset = -new_offset if self._logline.datetime.date() > self._date else new_offset
+
+      self.seek(new_offset, 1)
+      if self.tell() == self._line_pos:
+        # Already read from here
+        return False
+
+      return self.recursive_jumps_to_date(abs(new_offset))
+
+  def back_rewind(self):
+    offset = LogFile.INITIAL_OFFSET
+    found = False
+    while True:
+      # offset from last ok line
+      new_pos = self._line_pos-offset
+      if new_pos<0:
+        new_pos = 0
+      self.seek(new_pos, 0)
+
+      if not self.read_until_logline():
+        # impossible
+        raise Exception("unable to find line, even previously found")
+
+      if self._logline.datetime.date()<self._date:
+        # found prev date
+        found = True
+        break
+      if new_pos == 0:
+        # begin was already reached, stop
+        break      
+      if offset<LogFile.MAX_FAST_GROW:
+        offset *= 2 # fast jumps
+      else:
+        offset += LogFile.INCREMENTAL_GROW # incremental jumps
+    return found 
+
+  def forward_rewind(self):
+    while True:
+      if not self.read_until_logline():
+        # impossible
+        raise Exception("unable to find line, even previously found")
+      if self._logline.datetime.date()==self._date:
+        # found
+        return True
+    
+  @property
+  def filename(self):
+    return self._filename
+    
+  @property
+  def logline(self):
+    return self._logline
+    
+  @property
+  def date(self):
+    return self._date
+    
+  @date.setter
+  def date(self, _date):
+    self._date = _date
 
 
+
+# Get params
 if not len(sys.argv)==4:
   print "usage: {0} <LOG FILENAME> <MONTH> <DAY>".format(sys.argv[0])
   sys.exit(1)
@@ -50,61 +136,28 @@ except:
   print "wrong date"
   sys.exit(1)
 
+# Check
 source_filesize = os.path.getsize(log_filename)
-
 if source_filesize == 0:
   print "empty file"
-  sys.exit()
+  sys.exit(1)
 
-with open(log_filename, "rb") as source:
-  start_pos = source_filesize/2
-  source.seek(start_pos, 1)
-  date_pos = find_date(source, start_pos)
-  if date_pos<0:
-    print "not found"
-    sys.exit()
+# Searching
+with LogFile(log_filename) as lf:
+  lf.date = target_date
+  if not lf.fast_rewind():
+    print "date does not exist in file"
+    sys.exit(1)
 
-  o = 1024
-  found = False
-  while True:
-    offset = date_pos-o
-    if offset<0:
-      offset = 0
-    source.seek(offset, 0)
+  if not lf.back_rewind():
+    # last read date is the lowest
+    print str(lf.logline.datetime)  
 
-    line_pos, logline = read_until_logline(source)
-    if line_pos<0:
-      print "strange situation"
-      sys.exit()
-
-    print "reading back..."
-    print str(logline.datetime)
-
-    if logline.datetime.date()<target_date:
-      found = True
-      print "found prev date..."
-      break
-    
-    if offset == 0:
-      print "begin reached..."
-      break
-      
-    o += o*2 if o<256000 else o
-    print o
-  
-  if not found:
-    print "prev date not found..."
-    print str(logline.datetime)  
+  if lf.forward_rewind():
+    # found first occurence of date
+    print str(lf.logline.datetime)
   else:
-    while True:
-      line_pos, logline = read_until_logline(source)        
-      if line_pos<0:
-        print "strange situation"
-        sys.exit()
-      if logline.datetime.date()==target_date:
-        print "found..."
-        print str(logline.datetime)
-        break
-      
+    # impossible
+    raise Exception("cannot return to date")
       
       
